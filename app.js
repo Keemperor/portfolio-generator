cdd5763d0d7cd59332b0d0ddea262ba1a811fd04
Keemperor <jacobotau@hotmail.com>
2021-12-19 01:48:25 -0500

    interfaced with inquirer

--- app.js
+++ app.js
@@ -53,7 +53,9 @@ printProfileData(profileDataArgs);*/
 //console.log(name, github);
 //console.log(generatePage(name, github));
 
-const fs = require ('fs');
+const inquirer = require('inquirer');
+
+/*const fs = require ('fs');
 
 const generatePage = require('./src/page-template.js');
 
@@ -61,11 +63,21 @@ const  profileDataArgs = process.argv.slice(2);
 
 const [name, github] = profileDataArgs;
 
-fs.writeFile('index.html',  generatePage(name, github), err  => {
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('index.html',  pageHTML, err  => {
   if (err) throw err;
   console.log('Portfolio complete! Check out index.html to see the output');
-});
+});*/
 
+inquirer
+.prompt([
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?'
+  }
+])
+.then(answers => console.log(answers));
 
-
-
+//console.log(inquirer);
