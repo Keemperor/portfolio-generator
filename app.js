0089f0167359968a147430f35ef1e926f1ae174a
Keemperor <jacobotau@hotmail.com>
2021-12-19 01:09:11 -0500

    created webpage and file system

--- app.js
+++ app.js
@@ -29,11 +29,11 @@ personObj.hobbies = 'Warhammer 40K'
 console.log(animalArray);
 console.log(personObj);*/
 
-const  profileDataArgs = process.argv.slice(2, process.argv.length);
+// const  profileDataArgs = process.argv.slice(2, process.argv.length);
 //console.log(profileDataArgs);
 
 
-const printProfileData = profileDataArr => {
+/*const printProfileData = profileDataArr => {
   // This...
   for (let i = 0; i < profileDataArr.length; i += 1) {
     console.log(profileDataArr[i]);
@@ -45,4 +45,27 @@ const printProfileData = profileDataArr => {
   profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
-printProfileData(profileDataArgs);
+printProfileData(profileDataArgs);*/
+
+//const name = profileDataArgs[0];
+//const github = profileDataArgs[1];
+
+//console.log(name, github);
+//console.log(generatePage(name, github));
+
+const fs = require ('fs');
+
+const generatePage = require('./src/page-template.js');
+
+const  profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('index.html',  generatePage(name, github), err  => {
+  if (err) throw err;
+  console.log('Portfolio complete! Check out index.html to see the output');
+});
+
+
+
+
